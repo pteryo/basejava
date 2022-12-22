17b15688a2ff4e5e5765e628d92a8322c64de254
pteryo <116794811+pteryo@users.noreply.github.com>
2022-12-22 21:14:15 +0300

    методы реализованы 001

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int lenght = 0;
 
     void clear() {
+        for (int i = 0; i <= lenght; i++) {
+            storage[i] = null;
+        }
+        lenght = 0;
     }
 
     void save(Resume r) {
+        storage[lenght++] = r;
     }
 
     Resume get(String uuid) {
+        int i = getPosition(uuid);
+        if (i >= 0) {
+            return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i = getPosition(uuid);
+        for (; i < lenght; i++) {
+            storage[i] = storage[i + 1];
+        }
+        if (i > 0) {
+            storage[lenght] = null;
+        }
+        lenght--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, lenght);
     }
 
     int size() {
-        return 0;
+        return lenght;
+    }
+
+    private int getPosition(String uuid) {
+        for (int i = 0; i < lenght; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
