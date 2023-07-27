1c958a5741f0c88913889800c6c0e15e3bc4da2a
Pavel Teryoshin <116794811+pteryo@users.noreply.github.com>
2023-07-27 16:03:45 +0300

    HW06 MapStorage теперь делает map по резюме.

--- src/com/learnjava/storage/MapStorage.java
+++ src/com/learnjava/storage/MapStorage.java
@@ -11,33 +11,33 @@ public class MapStorage extends AbstractStorage {
     private final Map<String, Resume> map = new HashMap<>();
 
     @Override
-    protected String getSearchKey(String uuid) {
-        return uuid;
+    protected Resume getSearchKey(String uuid) {
+        return map.get(uuid);
     }
 
     @Override
     protected void doUpdate(Resume r, Object searchKey) {
-        map.put((String) searchKey, r);
+        map.put(r.getUuid(), r);
     }
 
     @Override
     protected boolean isExist(Object searchKey) {
-        return map.containsKey((String) searchKey);
+        return searchKey != null;
     }
 
     @Override
     protected void doSave(Resume r, Object searchKey) {
-        map.put((String) searchKey, r);
+        map.put(r.getUuid(), r);
     }
 
     @Override
     protected Resume doGet(Object searchKey) {
-        return map.get((String) searchKey);
+        return (Resume) searchKey;
     }
 
     @Override
     protected void doDelete(Object searchKey) {
-        map.remove((String) searchKey);
+        map.remove(((Resume) searchKey).getUuid());
     }
 
     @Override
@@ -45,11 +45,11 @@ public class MapStorage extends AbstractStorage {
         map.clear();
     }
 
-
     @Override
     public List<Resume> doCopyAll() {
         return new ArrayList<>(map.values());
     }
+
     @Override
     public int size() {
         return map.size();
