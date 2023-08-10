f7a533e3f9582343768434b86b04f640ab741bd3
pteryo <116794811+pteryo@users.noreply.github.com>
2023-08-10 16:27:49 +0300

    HW12 больше стримов.

--- src/com/learnjava/HomeWork12Exercise.java
+++ src/com/learnjava/HomeWork12Exercise.java
@@ -1,45 +1,29 @@
 package com.learnjava;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class HomeWork12Exercise {
     public static int minValue(int[] values) {
-        int answer = 0;
-        int[] uniqueFigure = new int[10];
-        IntStream inputStream = Arrays.stream(values);
-        inputStream.forEach(num -> uniqueFigure[num] = 1);
-        for (int i = 1; i < 10; i++) {
-            if (uniqueFigure[i] == 1) {
-                answer = answer * 10 + i;
-            }
-        }
-        return answer;
+        return Arrays.stream(values).sorted().distinct().reduce(0, (subResult, currentNum) -> subResult * 10 + currentNum);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
-        List<Integer> even = new ArrayList<>();
-        List<Integer> odd = new ArrayList<>();
-        integers.forEach(integer -> {
-            if (integer % 2 == 0) {
-                even.add(integer);
-            } else {
-                odd.add(integer);
-            }
-        });
-        if (odd.size() % 2 == 0) {
-            return odd;
+        List<List<Integer>> oddEven = integers.stream().collect(Collectors.teeing(
+                Collectors.filtering(num -> num % 2 == 0, Collectors.toList()),
+                Collectors.filtering(num -> num % 2 != 0, Collectors.toList()), List::of));
+        if (oddEven.get(0).size() % 2 == 0) {
+            return oddEven.get(1);
         } else {
-            return even;
+            return oddEven.get(0);
         }
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 1, 4, 5, 5, 9, 8};
         System.out.println("Минимальное значение по условиям задачи: " + minValue(arr));
-        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> intList = Arrays.asList(1, 2, 3, 7, 8);
         System.out.println("четные или не четные числа: " + oddOrEven(intList));
     }
 }
