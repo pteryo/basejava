78849472e8b2520eaed8e6d602db6d5247123ad9
pteryo <116794811+pteryo@users.noreply.github.com>
2023-08-12 20:03:27 +0300

    HW12 в первом упражнении разбито на несколько строк. Во втором использован Collectors.partitioningBy

--- src/com/learnjava/HomeWork12Exercise.java
+++ src/com/learnjava/HomeWork12Exercise.java
@@ -1,18 +1,24 @@
 package com.learnjava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class HomeWork12Exercise {
     public static int minValue(int[] values) {
-        return Arrays.stream(values).sorted().distinct().reduce(0, (subResult, currentNum) -> subResult * 10 + currentNum);
+        return Arrays.stream(values)
+                .sorted()
+                .distinct()
+                .reduce(0, (subResult, currentNum) -> subResult * 10 + currentNum);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
-        List<List<Integer>> oddEven = integers.stream().collect(Collectors.teeing(
-                Collectors.filtering(num -> num % 2 == 0, Collectors.toList()),
-                Collectors.filtering(num -> num % 2 != 0, Collectors.toList()), List::of));
+
+        Map<Boolean, List<Integer>> map = integers.stream()
+                .collect(Collectors.partitioningBy(num -> num % 2 == 0));
+        List<List<Integer>> oddEven = new ArrayList<>(map.values());
         if (oddEven.get(0).size() % 2 == 0) {
             return oddEven.get(1);
         } else {
@@ -23,7 +29,7 @@ public class HomeWork12Exercise {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 1, 4, 5, 5, 9, 8};
         System.out.println("Минимальное значение по условиям задачи: " + minValue(arr));
-        List<Integer> intList = Arrays.asList(1, 2, 3, 7, 8);
+        List<Integer> intList = Arrays.asList(1, 2, 3, 7, 8, 3);
         System.out.println("четные или не четные числа: " + oddOrEven(intList));
     }
 }
